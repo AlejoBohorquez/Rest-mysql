df4a7005bdb754e4360434341a87d391fe99720b
Alejandro Bohorquez <autolinesoporte@gmail.com>
2021-11-29 21:25:23 -0500

    Add config db

--- src/database.js
+++ src/database.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Vip2021*',
-    database: 'company'
+    host: 'us-cdbr-east-04.cleardb.com', 
+    user: 'b75c693e9afe5f', // root', 
+    password: 'db534960', //'Vip2021*',
+    database: 'heroku_c75993627561ab7'//'company'
 });
+
 
 mysqlConnection.connect(function (err){
     if(err){
