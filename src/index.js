e5eda41071a2ec3007dcc77d3dfa19b3e9d5b1e2
Alejandro Bohorquez <autolinesoporte@gmail.com>
2021-12-30 12:59:54 -0500

    profile

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-'use strict';
-
 require('dotenv').config()
 
 const express = require('express');
 const app = express();
 
 //Settings
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 
 
 //Middlewares
@@ -15,10 +13,7 @@ app.use(express.json());
 //Routes
 app.use(require('./routes/persona'));
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log('Express server listening on port', port)
+
+app.listen(process.env.PORT, () => {
+    console.log('Server on port', process.env.PORT);
 });
-// app.listen(process.env.PORT, () => {
-//     console.log('Server on port', process.env.PORT);
-// });
